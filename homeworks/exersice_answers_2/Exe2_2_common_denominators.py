1c4bddc9e2ec7a294c0604bb9cbd349b2a3df9c0
moshekagan <shaikagan!1>
2019-11-22 16:54:28 +0200

    ex2 solution

--- homeworks/exersice_answers_2/Exe2_2_common_denominators.py
+++
@@ -1,8 +0,0 @@
-number = int(input("please insert a number: "))
-first_denominator = int(input("please insert the first denominator: "))
-second_denominator = int(input("please insert the second denominator: "))
-
-is_common_denominators = number % first_denominator == 0 and number % second_denominator == 0
-
-print("Are the numbers " + str(first_denominator) + " and " + str(second_denominator) + " are common denominators of " + str(number) + "? " +
-      str(is_common_denominators is True))
