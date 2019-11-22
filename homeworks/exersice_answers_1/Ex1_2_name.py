fdbc9089a481bdd8caf7308086e6ec9de2f7a48c
moshekagan <shaikagan!1>
2019-11-22 16:54:06 +0200

    ex1 solution

--- homeworks/exersice_answers_1/Ex1_2_name.py
+++
@@ -1,7 +0,0 @@
-LINE = "#################"
-
-name = input("Please insert your full name: ")
-
-print(LINE)
-print('# ' + name + ' #')
-print(LINE)
--- homeworks/exersice_answers_1/Ex1_3_circle.py
+++
@@ -1,9 +0,0 @@
-PI = 3.14
-
-radius = int(input("Please insert a circle radius: "))
-
-circumference = 2 * PI * radius
-area = PI * radius * radius
-
-print("The circumference is: " + str(circumference))
-print("The area is: " + str(area))
