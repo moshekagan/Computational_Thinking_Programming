21d2ddb5e66adca3c31e6d906c4ebb65bd211c1b
moshekagan <shaikagan!1>
2019-12-01 01:48:21 +0200

    comments

--- homeworks/check_homeworks/comments.py
+++ homeworks/check_homeworks/comments.py
@@ -1,4 +1,4 @@
-WRONG_ZIP_FILE_FORMAT = " [%s] Wrong zip file format (should be like: EX#_FirsName_LastName_ID.zip)"
+WRONG_ZIP_FILE_FORMAT = " -5 [%s] Wrong zip file format (should be like: EX#_FirsName_LastName_ID.zip)"
 SHOULD_BE_SUBMITTED_ONLY_1_ZIP_FILE = " Should submit only 1 zip file"
 PYTHON_FILE_DOES_NOT_EXIST = " Python file does not exist [%s]"
 CANT_EXTRACT_ZIP_FILE = " Can't extract zip file"
@@ -10,6 +10,11 @@ CONST_IS_NOT_CAMELCASE = " -2 Const should be written in CAPITAL_LETTERS"
 DIDNT_USE_CONSTS = " -2 You didn't use constants"
 MEANIGFULL_NAMES = " -2 Use meaningful names to your variables"
 
-
 WRONG_OUTPUT = " -5 Wrong output - ex"
 PROGRAM_FAILED = " -5 Program failed - "
+NOT_IMPLEMENT = " -10 didn't implement - "
+
+ONLY_PY_FILES = "-3 include only python files (.py) and do not include venv directory or any other files!"
+DIDNT_FOLLOW_EXAMPLE = " -2 you didn't follow example inputs"
+SUBMISSION_LATE = "-5 Submission late"
+CONST_VARS = "const - CAPITAL_LETTERS variables - small_letters"
