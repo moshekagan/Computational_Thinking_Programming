bd12b90ee70fe12543887d1dd0d2194067f0cf6f
moshekagan <shaikagan!1>
2019-12-01 20:41:59 +0200

    fix typo

--- homeworks/check_homeworks/comments.py
+++ homeworks/check_homeworks/comments.py
@@ -1,12 +1,12 @@
 WRONG_ZIP_FILE_FORMAT = " -5 [%s] Wrong zip file format (should be like: EX#_FirsName_LastName_ID.zip)"
-SHOULD_BE_SUBMITTED_ONLY_1_ZIP_FILE = " Should submit only 1 zip file"
+SHOULD_BE_SUBMITTED_ONLY_1_ZIP_FILE = " -5 Should submit only 1 zip file"
 PYTHON_FILE_DOES_NOT_EXIST = " Python file does not exist [%s]"
 CANT_EXTRACT_ZIP_FILE = " Can't extract zip file"
 
 WRONG_FILE_NAME = "-3 wrong file name"
 
 BAD_SUBMISSION_STRUCTURE = " -10 Wrong submission structure "
-CONST_IS_NOT_CAMELCASE = " -2 Const should be written in CAPITAL_LETTERS"
+CONST_IS_NOT_CAMELCASE = " -2 Consts should be written in CAPITAL_LETTERS"
 DIDNT_USE_CONSTS = " -2 You didn't use constants"
 MEANIGFULL_NAMES = " -2 Use meaningful names to your variables"
 
