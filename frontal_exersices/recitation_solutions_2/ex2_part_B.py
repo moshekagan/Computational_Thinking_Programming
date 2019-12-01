891f98f8db1112b75acf81a708011adc7353b603
moshekagan <shaikagan!1>
2019-12-01 20:42:17 +0200

    fix

--- frontal_exersices/recitation_solutions_2/ex2_part_B.py
+++ frontal_exersices/recitation_solutions_2/ex2_part_B.py
@@ -1,15 +1,19 @@
-number = int(input("Insert a 3 digits number: "))
+number = input("Insert a 3 digits number: ")
 
-# number = 213
-first_digit = number // 100 # 2
-second_digit = number // 10 % 10 # 21 % 10 -> 1
-third_digit = number % 10 # 3
-
-is_middle_greater = second_digit > first_digit and second_digit > third_digit # False
-is_middle_smaller = second_digit < first_digit and second_digit < third_digit # True
-
-if is_middle_greater or is_middle_smaller:
-    print(str(number) + " is extreme number.")
+if len(number) != 3 or not number.isdigit():
+    print("Invalid input")
 else:
-    print(str(number) + " is NOT extreme number.")
+    number = int(number)
+
+    first_digit = number // 100
+    second_digit = number // 10 % 10
+    third_digit = number % 10
+
+    is_middle_greater = second_digit > first_digit and second_digit > third_digit
+    is_middle_smaller = second_digit < first_digit and second_digit < third_digit
+
+    if is_middle_greater or is_middle_smaller:
+        print(str(number) + " is extreme number.")
+    else:
+        print(str(number) + " is NOT extreme number.")
 
