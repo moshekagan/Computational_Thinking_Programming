4b475fea337c2c111f4b38867150567cafb507d5
moshekagan <shaikagan!1>
2019-12-21 03:24:29 +0200

    ex4 config

--- homeworks/check_homeworks/config.py
+++ homeworks/check_homeworks/config.py
@@ -3,20 +3,21 @@ import time
 BASE_DIR = '/Users/moshekagan/Documents/idc/submitions'
 
 # INPUTS:
-ZIP_FILE_NAME = 'Computational Thinking and Programming-26002-20201 - 200583-Exercise #3-30848.zip'
-NUMBER_OF_ASSIGNMENT = 3
+ZIP_FILE_NAME = 'Computational Thinking and Programming-26002-20201 - 200583-Exercise #4-32977.zip'
+NUMBER_OF_ASSIGNMENT = 4
 
 SUB_DIR_NAME = 'Exercise_' + str(NUMBER_OF_ASSIGNMENT)
 BASE_SUB_DIR = BASE_DIR + "/" + SUB_DIR_NAME
 MAIN_ZIP_FILE = BASE_DIR + "/" + ZIP_FILE_NAME
 
-DO_IT_FROM_BEGIN = True
+DO_IT_FROM_BEGIN = False
 CHECK_EACH_STUDENT = True
 
 ASSIGNMENTS_NAMES_AND_INPUTS = {
-    "Ex3_1_practice_basics.py": [None],
-    "Ex3_2_seasons.py": [[11012019], [11081990], [31091000]],
-    "Ex3_3_resturant.py": [[4, 1, 1, 1, 0.1, "True", "False"], [45, 1, 2, 1, 0.12, "False", "False"], [1, 1, 1, 1, 0, "True", "True"], [1, 1, 1, 1, 0, "False", "True"]]
+    "Ex4_1_abs.py": [[5, 10], [5, 5]],
+    "Ex4_2_shares.py": [[5, 'no', 1], [21, 'yes', 10], [20, 'yess', 100], [35, 'yes', 6]],
+    "Ex4_3_square_loop.py": [[7]],
+    "Ex4_4_seven_boom .py": [[15]]
 }
 
 CSV_FILE_PATH = BASE_SUB_DIR + "/students_res_%s.csv" % NUMBER_OF_ASSIGNMENT
