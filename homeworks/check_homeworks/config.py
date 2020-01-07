4a9e13af8561ea78830125948933c6b9a3edf717
moshekagan <shaikagan!1>
2020-01-08 00:36:18 +0200

    config ex6

--- homeworks/check_homeworks/config.py
+++ homeworks/check_homeworks/config.py
@@ -3,8 +3,8 @@ import time
 BASE_DIR = '/Users/moshekagan/Documents/idc/submitions'
 
 # INPUTS:
-ZIP_FILE_NAME = 'Computational Thinking and Programming-26002-20201 - 200583-Exercise #5-35228.zip'
-NUMBER_OF_ASSIGNMENT = 5
+ZIP_FILE_NAME = 'Computational Thinking and Programming-26002-20201 - 200583-Exercise #6-37499.zip'
+NUMBER_OF_ASSIGNMENT = 6
 
 SUB_DIR_NAME = 'Exercise_' + str(NUMBER_OF_ASSIGNMENT)
 BASE_SUB_DIR = BASE_DIR + "/" + SUB_DIR_NAME
@@ -14,10 +14,9 @@ DO_IT_FROM_BEGIN = False
 CHECK_EACH_STUDENT = True
 
 ASSIGNMENTS_NAMES_AND_INPUTS = {
-    "Ex5_1_length.py": [[123456], [1]],
-    "Ex5_2_average_a.py": [[4, 2, 2, 0, 3], [0]],
-    "Ex5_3_average_b.py": [[2, 3, 0, 2, "end"], ["end"]],
-    "Ex5_4_refactor.py": [[123321], [1213]],
+    "Ex6_1_hourglass.py": [[3], [-9, 0, 5]],
+    "Ex6_2_carpet.py": [[2, '@'], [4, '~']],
+    "Ex6_3_tree.py": [[3], [4]]
 }
 
 CSV_FILE_PATH = BASE_SUB_DIR + "/students_res_%s.csv" % NUMBER_OF_ASSIGNMENT
