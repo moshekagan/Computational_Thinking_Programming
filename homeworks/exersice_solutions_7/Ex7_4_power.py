ffbc96990cafa9a7c9e5d20546ab39892ffa11ef
moshekagan <shaikagan!1>
2020-01-09 16:26:32 +0200

    fix power algo

--- homeworks/exersice_solutions_7/Ex7_4_power.py
+++ homeworks/exersice_solutions_7/Ex7_4_power.py
@@ -2,14 +2,21 @@ base = int(input("Base: "))
 power = int(input("Power: "))
 
 if power == 0:
-    print(1)
+    increment = 1
+else:
+    positive_base = base
+    if positive_base < 0:
+        positive_base = positive_base * -1
 
-answer = base
-increment = base
+    answer = positive_base
+    increment = positive_base
 
-for i in range(0, power - 1):
-    for j in range(0, base - 1):
-        answer = answer + increment
-    increment = answer
+    for i in range(0, power - 1):
+        for j in range(0, positive_base - 1):
+            answer = answer + increment
+        increment = answer
+
+    if base < 0 and power % 2 != 0:
+        increment = increment * -1
 
 print(str(base) + "^" + str(power) + " = " + str(increment))
